3a2dc81f069eb32459ee4db86f3c0fa6e89aad17
jatpac501 <jatpac501@gmail.com>
2024-03-01 22:50:05 +0400

    Replace Dashboard-link to Chats-link

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,10 +4,10 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+    <AppLayout title="Chats">
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Dashboard
+                Chats
             </h2>
         </template>
 
